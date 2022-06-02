d89100085d6e0ac300c61d05d13ab440a49fa717
Kate Glazko <kglazko@Kates-MacBook-Pro.local>
2022-06-02 13:58:55 -0700

    Front end adding

---
+++ scripts/mlrun.js
@@ -0,0 +1,9 @@
+function showScores() {
+  alert('I am foo!');
+  var firstScore = document.getElementById('first-score');
+  firstScore.innerHTML += "46%";
+
+  var secondScore = document.getElementById('second-score');
+  secondScore.innerHTML += "96%";
+
+}
